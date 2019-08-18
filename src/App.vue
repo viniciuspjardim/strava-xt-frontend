1c787462cb05e38fa9ec4d6833e61c9bd97ca453
Vinícius Jardim <viniciuspjardim@gmail.com>
2019-08-17 22:00:37 -0300

    Added support to async/await

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,11 @@
         {{ activity }}
       </li>
     </ul>
-    <p v-else>No activities loaded.</p>
-    <button class="btn btn-primary my-3" @click="loadActivities">Load Activities</button>
+    <div v-else :class="messageClass" role="alert">
+      {{ message.text }}
+    </div>
+    <hr>
+    <button class="btn btn-primary my-1" @click="loadActivities">Load Activities</button>
   </div>
 </template>
 
@@ -18,21 +21,34 @@
   export default {
     data() {
       return {
+        message: {
+          text: 'No activityes loaded.',
+          type: 'alert'
+        },
         activities: []
       }
     },
     methods: {
-      loadActivities() {
-        this.$http.get('')
-          .then(res => {
-            return res.json();
-          })
-          .then(data => {
-            data.forEach(activity => {
-              this.activities.push(activity.name);
-            });
-          }
-        );
+      async loadActivities() {
+        try {
+          let res = await this.$http.get('');
+          let data = await res.json();
+          this.activities.length = 0;
+
+          data.forEach(activity => {
+            this.activities.push(activity.name);
+          });
+        }
+        catch(err) {
+          console.log(err);
+          this.message.text = 'Error loading activities!';
+          this.message.type = 'error';
+        }
+      }
+    },
+    computed: {
+      messageClass() {
+        return this.message.type == 'error' ? 'alert alert-danger' : 'alert alert-primary';
       }
     }
   };
