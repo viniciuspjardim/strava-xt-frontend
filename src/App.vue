40709556b2ca5fd94df90aadc67f9e92432d0fb9
Vinícius Jardim <viniciuspjardim@gmail.com>
2019-08-18 03:29:38 -0300

    Added data filter and pagination in activities

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="container">
     <h1>Strava Activities</h1>
+    <form class="form-inline mt-3">
+      <div class="form-group mr-sm-3 mb-2">
+        <label class="mr-sm-2">Max date</label>
+        <input class="form-control" type="text" v-model="maxDate">
+      </div>
+      <div class="form-group mx-sm-3 mb-2">
+        <label class="mr-sm-2">Min date</label>
+        <input class="form-control" type="text" v-model="minDate">
+      </div>
+      <div class="form-group mx-sm-3 mb-2">
+        <button class="btn btn-primary" @click.prevent="loadActivities(1)">Load Activities</button>
+      </div>
+    </form>
+    <hr>
     <ul class="list-group" v-if="activities.length > 0">
       <li
         class="list-group-item"
@@ -12,15 +26,41 @@
     <div v-else :class="messageClass" role="alert">
       {{ message.text }}
     </div>
+    <br>
     <hr>
-    <button class="btn btn-primary my-1" @click="loadActivities">Load Activities</button>
+    <nav aria-label="Page navigation example">
+      <ul class="pagination">
+        <li class="page-item">
+          <a class="page-link" @click="loadActivities(page -1)" aria-label="Previous">
+            <span aria-hidden="true">&laquo;</span>
+            <span class="sr-only">Previous</span>
+          </a>
+        </li>
+        <li class="page-item"><a class="page-link" @click="loadActivities(1)">1</a></li>
+        <li class="page-item"><a class="page-link" @click="loadActivities(2)">2</a></li>
+        <li class="page-item"><a class="page-link" @click="loadActivities(3)">3</a></li>
+        <li class="page-item">
+          <a class="page-link" @click="loadActivities(page +1)" aria-label="Next">
+            <span aria-hidden="true">&raquo;</span>
+            <span class="sr-only">Next</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <p>{{ polyline }}</p>
   </div>
 </template>
 
 <script>
+  import moment from 'moment';
+
   export default {
     data() {
       return {
+        page: 1,
+        maxDate: null,
+        minDate: null,
+        polyline: 'Sem Linha',
         message: {
           text: 'No activityes loaded.',
           type: 'alert'
@@ -29,15 +69,34 @@
       }
     },
     methods: {
-      async loadActivities() {
+      async loadActivities(page) {
         try {
-          let res = await this.$http.get('');
+          this.page = page;
+          let before = moment(this.maxDate, 'DD/MM/YYYY');
+          let after = moment(this.minDate, 'DD/MM/YYYY');
+
+          let params = {
+            page,
+            before: before.isValid() ? before.format('X') : null,
+            after: after.isValid() ? after.format('X') : null
+          };
+
+          let res = await this.$http.get('activities/', { params });
           let data = await res.json();
-          this.activities.length = 0;
+          this.activities = [];
+
+          console.log(data);
+          this.polyline = data[0].map.summary_polyline;
 
           data.forEach(activity => {
-            this.activities.push(activity.name);
+            let date = moment(new Date(activity.start_date)).format('DD/MM/YYYY');
+            this.activities.push(`${activity.name} - ${date}`);
           });
+
+          if(this.activities.length == 0) {
+            this.message.text = 'No activityes found.';
+            this.message.type = 'alert';
+          }
         }
         catch(err) {
           console.log(err);
