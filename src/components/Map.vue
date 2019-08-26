cc57cbf2dad1e1fce5b8ef33a50363e8cbf80e7a
Vinícius Jardim <viniciuspjardim@gmail.com>
2019-08-26 04:36:11 -0300

    Map component created

---
+++ src/components/Map.vue
@@ -0,0 +1,33 @@
+<template>
+  <app-map-loader
+    :mapConfig="mapConfig"
+    apiKey=""
+    class="gmap">
+  </app-map-loader>
+</template>
+
+<script>
+  import MapLoader from "./MapLoader";
+  import { mapSettings } from "../constants/mapSettings";
+
+  export default {
+    components: {
+      appMapLoader: MapLoader
+    },
+
+    computed: {
+      mapConfig() {
+        return {
+          ...mapSettings,
+          center: { lat: 5, lng: 99 }
+        };
+      }
+    }
+  };
+</script>
+
+<style scoped>
+  .gmap {
+    height: 600px;
+  }
+</style>
