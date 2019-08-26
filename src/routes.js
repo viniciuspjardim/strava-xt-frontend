a87ed76f08f65fb8b0e1b1127c398e9d8473a225
Vinícius Jardim <viniciuspjardim@gmail.com>
2019-08-26 04:39:56 -0300

    Routes to new components added

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,13 @@
 import Home from './components/Home';
 import Activities from './components/Activities';
+import Activity from './components/Activity';
+import GearCalculator from './components/GearCalculator';
+import Map from './components/Map';
 
 export const routes = [
   { path: '', component: Home },
-  { path: '/activities', component: Activities }
+  { path: '/activities', component: Activities },
+  { path: '/activities/:id', component: Activity },
+  { path: '/gear', component: GearCalculator },
+  { path: '/map', component: Map }
 ];
